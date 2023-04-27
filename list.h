452d90773ced822ee31a17c249383c172c6081a7
Rafael <kamaltdinov@inbox.ru>
2023-04-27 15:03:51 +0500

    add list logic

---
+++ list.h
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SERVICES 14
+#define SERVICE_NAME_LENGTH 8
+
+struct row {
+    char last_name[20];
+    char first_name[20];
+    char middle_name[20];
+    char number[15];
+    char services[MAX_SERVICES][SERVICE_NAME_LENGTH];
+};
+
+struct node {
+    struct row data;
+    struct node* next;
+};
+
+void insert_front(struct node** head, struct row data) {
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    if (new_node == NULL) {
+        printf("Error allocating memory\n");
+        exit(1);
+    }
+    new_node->data = data;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+void insert_back(struct node** head, struct row data) {
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    if (new_node == NULL) {
+        printf("Error allocating memory\n");
+        exit(1);
+    }
+    new_node->data = data;
+    new_node->next = NULL;
+    if (*head == NULL) {
+        *head = new_node;
+    } else {
+        struct node* curr = *head;
+        while (curr->next != NULL) {
+            curr = curr->next;
+        }
+        curr->next = new_node;
+    }
+}
+
+
+struct node* get_node(struct node* head, int n) {
+    int counter = 0;
+    while (counter < n && head) {
+        head = head->next;
+        counter++;
+    }
+    return head;
+}
+
+struct row pop(struct node **head) {
+    struct node* prev = NULL;
+    struct row val;
+    if (head == NULL) {
+        exit(-1);
+    }
+    prev = (*head);
+    val = prev->data;
+    (*head) = (*head)->next;
+    free(prev);
+    return val;
+}
+
+struct row delete_node(struct node** head, int n) {
+    if (n == 0) {
+        return pop(head);
+    } else {
+        struct node *prev = get_node(*head, n-1);
+        struct node *elm  = prev->next;
+        struct row val = elm->data;
+ 
+        prev->next = elm->next;
+        free(elm);
+        return val;
+    }
+}
+
+void print_list(struct node* head) {
+    while (head != NULL) {
+        printf("%s %s %s, номер %s, услуги:", head->data.last_name, head->data.first_name, head->data.middle_name, head->data.number);
+        for (int j = 0; j < MAX_SERVICES; j++) {
+            if (head->data.services[j][0] != '\0') {
+                printf(" %s", head->data.services[j]);
+            } else {
+                break;
+            }
+        }
+        printf("\n");
+        head = head->next;
+    }
+}
+
+void free_list(struct node* head) {
+    while (head != NULL) {
+        struct node* temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+// void delete_node(struct node** head, int index) {
+//     if (*head == NULL) {
+//         return;
+//     }
+//     struct node* temp = *head;
+//     if (index == 0) {
+//         *head = temp->next;
+//         free(temp);
+//         return;
+//     }
+//     for (int i = 0; temp != NULL && i < index - 1; i++) {
+//         temp = temp->next;
+//     }
+//     if (temp == NULL || temp->next == NULL) {
+//         return;
+//     }
+//     struct node* next = temp->next->next;
+//     free(temp->next);
+//     temp->next = next;
+// }
+
+// int main() {
+//     struct node* head = NULL;
+
+//     // Добавление элементов в начало списка
+//     struct row row1 = {"Ivanov", "Ivan", "Ivanovich", "1234567890"};
+//     struct row row2 = {"Petrov", "Petr", "Petrovich", "0987654321"};
+//     insert_front(&head, row1);
+//     insert_front(&head, row2);
+//     printf("Список элементов, добавленных в начало:\n");
+//     print_list(head);
--- main.c
+++ main.c
@@ -1,83 +1,38 @@
-// Online C compiler to run C program online
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
+
+#include "list.h"
 
 #define MAX_SERVICES 14
 #define SERVICE_NAME_LENGTH 8
 
-struct row {
-    char last_name[20];
-    char first_name[20];
-    char middle_name[20];
-    char number[15];
-    char services[MAX_SERVICES][SERVICE_NAME_LENGTH];
-//     last_name:string;                                                                  // Фамилия владельца
-// first_name:string;                                                                     // Имя владельца
-// middle_name:string;                                                               // Отчество владельца
-// number:phone;                                                                         // Номер телефона
-// services:set of ['calls','sms','gprs','internet','s1','s2','s3',...,'s10'];
-};
-
-struct command{
-    int command; // 0 - select, 1 - insert
-    char* fileds; // first_name, last_name, number
-    char* cond; //  ""
-};
-
-int pars_command(const char* command, int strlen){
- 
-
-}
-
-
-void print_row(struct row r) {
-    printf("row: %s %s %s\n", r.last_name, r.first_name, r.middle_name);
-    printf("services: ");
-    for (int i=0; i<14; i++)
-        printf("%s ", r.services[i]);
-    printf("\n");
-}
-
-int read_db();
-
- 
-void main()
-{
-    struct row r;
-    printf("size of: %lu\n", sizeof(r));
-    strcpy(r.first_name, "sdfsdfd");
-    strcpy(r.last_name, "123123132");
-    for (int i=0; i<14; i++)
-        strcpy(r.services[i], "");
-    strcpy(r.services[0], "calls");
-    strcpy(r.services[1], "sms");
-    strcpy(r.services[3], "gprs");
-    // r.last_name = "3r234234";
-    // print_row(r);
-
-    read_db();    
-
-    return;
-}
-
-int read_db() {
+struct node* read_db() {
 
     FILE* file = fopen("DB.csv", "r");
     if (!file) {
         printf("Error opening file\n");
-        return 1;
+        return NULL;
     }
 
     char buffer[1024];
     struct row clients[100];
     int num_clients = 0;
 
+    int db_size = -1;
+    while (fgets(buffer, sizeof(buffer), file)) {
+        db_size++;
+    }
+
+    printf("число строк %d\n", db_size);
+    fclose(file);
+
+    file = fopen("DB.csv", "r");
+    
     // Чтение заголовка
     fgets(buffer, sizeof(buffer), file);
+
+    struct node* head = NULL;
 
     // Чтение строк
     while (fgets(buffer, sizeof(buffer), file)) {
@@ -108,24 +63,47 @@ int read_db() {
                 i++;
             }
 
+            insert_back(&head, clients[num_clients]);
+            
             num_clients++;
         }
     }
 
-    // Вывод информации о клиентах
-    for (int i = 0; i < num_clients; i++) {
-        // printf("%s\n", clients[i].number);
-        printf("%s %s %s, номер: %s, услуги:", clients[i].last_name, clients[i].first_name, clients[i].middle_name, clients[i].number);
-        for (int j = 0; j < MAX_SERVICES; j++) {
-            if (clients[i].services[j][0] != '\0') {
-                printf(" %s", clients[i].services[j]);
-            } else {
-                break;
-            }
-        }
-        printf("\n");
-    }
+    // print_list(head);
 
     fclose(file);
-    return 0;
+    return head;
 }
+ 
+void main()
+{
+    struct row r;
+    printf("size of: %lu\n", sizeof(r));
+    strcpy(r.first_name, "sdfsdfd");
+    strcpy(r.last_name, "123123132");
+    for (int i=0; i<14; i++)
+        strcpy(r.services[i], "");
+    strcpy(r.services[0], "calls");
+    strcpy(r.services[1], "sms");
+    strcpy(r.services[3], "gprs");
+    // r.last_name = "3r234234";
+    // print_row(r);
+
+    struct node* head = read_db();  
+    print_list(head);
+
+    delete_node(&head, 1);
+    printf("\nAfter deleting\n");
+
+    print_list(head);
+
+    struct row temp = {"Hello", "World", "!", "89173456789", {"s1","s2","s333"}};
+    insert_back(&head, temp);
+
+    printf("\nAfter inserting\n");
+
+    print_list(head);
+
+    return;
+}
+
