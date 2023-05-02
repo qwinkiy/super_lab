35a706a52175a5359716474cf0de25ccdd22ffd2
Geralt1123 <97812612+Geralt1123@users.noreply.github.com>
2023-05-02 16:26:04 +0500

    Add files via upload
    update insert

--- insert.h
+++ insert.h
@@ -21,36 +21,54 @@ struct row* row_to_insert(const char* fields){
         service = strtok(NULL, del_serv);
         i++;
     }
-    
+    int k1 = 0;
+    int k2 = 0;
+    int k3 = 0;
+    int k4 = 0;
+    int k5 = 0;
+    int k6 = 0;
     for (int j = 0; j < i; j++) {
         const char* del_serv = "=";
         char* field = strtok(single_fld[j], del_serv);
         char* value = strtok(NULL, del_serv);
         // printf("=== field = %s, value = %s\n", field,value);
-    
-        if (strcmp(field, "last_name") == 0) 
+
+        if (strcmp(field, "last_name") == 0) {
             strcpy(row->last_name, value);
-        else if (strcmp(field, "first_name") == 0) 
+            k1++;
+        }
+        else if (strcmp(field, "first_name") == 0) {
             strcpy(row->first_name, value);
-        else if (strcmp(field, "middle_name") == 0) 
+            k2++;
+        }
+        else if (strcmp(field, "middle_name") == 0) {
             strcpy(row->middle_name, value);
-        else if (strcmp(field, "number") == 0) 
+            k3++;
+        }
+        else if (strcmp(field, "number") == 0) {
             strcpy(row->number, value);
-        else if (strcmp(field, "bonus_id") == 0) 
+            k4++;
+        }
+        else if (strcmp(field, "bonus_id") == 0) {
             row->bonus_id = atoi(value);
-        else if (strcmp(field, "discount_id") == 0) 
+            k5++;
+        }
+        else if (strcmp(field, "discount_id") == 0) {
             row->discount_id = atoi(value);
+            k6++;
+        }
         else {
             printf("Insert error, unknown field\n");
             return NULL;
         }
     }
-
     for (int k=0; k < MAX_SERVICES; ++k) 
-        strncpy(row->services[k], "\0", 1);
-        
-    return row;
+        strncpy(row->services[k], "\0", 1);    
+    if ((k1==1) && (k2==1) && (k3==1) && (k4==1) && (k5==1) && (k6==1)){
+        return row;}
+    else return NULL;
 }
+
 
 void exec_insert(struct node* head, const char* fields) {
     struct row* r = row_to_insert(fields);
